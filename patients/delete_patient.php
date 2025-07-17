20b56c4511b87942dff86c74c725af2e81c08bbc
abhiopkrck <abhishekboyaneco23859@gmail.com>
2025-07-17 02:17:02 -0700

    Update delete_patient.php

--- patients/delete_patient.php
+++ patients/delete_patient.php
@@ -1,3 +1,10 @@
 <?php
-// Delete patient.php
-?>
+include '../config.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $conn->query("DELETE FROM patients WHERE id=$id");
+}
+
+header("Location: view_patients.php");
+exit();
